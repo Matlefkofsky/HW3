17fe83aae4a4cb989da30c9ed05aa15496c3e1b0
Mathew Lefkofsky <MatLefkofsky@Mathews-MacBook-Pro.local>
2016-11-09 17:39:49 -0500

    second version

--- twitterhw3a.py
+++ twitterhw3a.py
@@ -3,8 +3,29 @@
 # hashtags #UMSI-206 #Proj3 in the tweet.
 
 # You will demo this live for grading.
-
+import tweepy
 print("""No output necessary although you 
 	can print out a success/failure message if you want to.""")
 
-#another test
+def get_api(cfg):
+  auth = tweepy.OAuthHandler(cfg['consumer_key'], cfg['consumer_secret'])
+  auth.set_access_token(cfg['access_token'], cfg['access_token_secret'])
+  return tweepy.API(auth)
+
+def main():
+  cfg = { 
+  "consumer_key"        : "K8TBYVBBnz5IDUHQCv5iQRhiI",
+  "consumer_secret"     : "j8Y2BydLgNewQYN80z61ozzhafHhMaAeIBSKSnjYRBSPEz5Agg",
+  "access_token"        : "796398976190844928-QTvOn7z0vzCBfrWyTx8dXkHHhpL8BYk",
+  "access_token_secret" : "awAsuLWQwjZARUDf5Q9aO2L4Zv5UVzcLm9ZqQ9hc7C1qx" 
+  }
+  api = get_api(cfg)
+  tweet = "Testing 1231"
+  status = api.update_status(status=tweet) 
+  # Yes, tweet is called 'status' rather confusing
+
+if __name__ == "__main__":
+	main()
+
+
+
--- twitterhw3b.py
+++ twitterhw3b.py
@@ -6,7 +6,28 @@
 # 3) Print the average polarity of the results
 
 # Be prepared to change the search term during demo.
+import tweepy
+from textblob import TextBlob
 
+consumer_key = 'K8TBYVBBnz5IDUHQCv5iQRhiI'
+consumer_secret = 'j8Y2BydLgNewQYN80z61ozzhafHhMaAeIBSKSnjYRBSPEz5Agg'
+access_token = '796398976190844928-QTvOn7z0vzCBfrWyTx8dXkHHhpL8BYk'
+access_token_secret = 'awAsuLWQwjZARUDf5Q9aO2L4Zv5UVzcLm9ZqQ9hc7C1qx'
 
-print("Average subjectivity is")
-print("Average polarity is")
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+api = tweepy.API(auth)
+
+search_term = input("Enter a term to search on twitter: ")
+for each in tweepy.Cursor(api.search, q=search_term, result_type = 'recent', include_entities=True,lang = 'en').items(100):
+	results = api.search(q=search_term)
+
+for result in results:
+    print (result.text)
+text = TextBlob(each.text)
+subjectivity = text.sentiment.subjectivity
+polarity = text.sentiment.polarity
+
+print("Average subjectivity is",subjectivity)
+print("Average polarity is",polarity)
