4c9ee42c282c9aeb34a505d0cacbf4710cc2ba23
Mathew Lefkofsky <MatLefkofsky@Mathews-MacBook-Pro.local>
2016-11-10 16:33:54 -0500

    madlib done

--- madlibhw3.py
+++ madlibhw3.py
@@ -9,46 +9,31 @@
 # Deliverables:
 # 1) Print the orginal text (150 tokens)
 # 1) Print the new text
-# code developed by Jackie Cohen; revised by Paul Resnick
-# further revised by Colleen van Lent for Python3
-
 import nltk # requires some downloading/installing dependencies to use all its features; numpy is especially tricky to install
 import random
-
-# import nltk
-nltk.download('punkt')
-
+from nltk.book import *
 from nltk import word_tokenize,sent_tokenize
+print("START*******")
+
+txt = text2[:151]
 
 debug = False #True
 
-# get file from user to make mad lib out of
 if debug:
 	print ("Getting information from file madlib_test.txt...\n")
-fname = "madlibtest2.txt" # need a file with this name in directory
 
-f = open(fname, 'r')
-para = f.read()
-tokens = nltk.word_tokenize(para)
-print("TOKENS")
-print(tokens)
-tagged_tokens = nltk.pos_tag(tokens) # gives us a tagged list of tuples
-print("TAGGED TOKENS")
-print(tagged_tokens)
-if debug:
-	print ("First few tagged tokens are:")
-	for tup in tagged_tokens[:5]:
-		print (tup)
+tagged_tokens = nltk.pos_tag(txt) 
 
-tagmap = {"NN":"a noun","NNS":"a plural noun","VB":"a verb","JJ":"an adjective"}
-substitution_probabilities = {"NN":.1,"NNS":.2,"VB":.25,"JJ":.25}
+
+tagmap = {"NN":"a noun","NNS":"a plural noun","RB":"adverb","JJ":"an adjective","VBD":"a past-tense verb"}
+substitution_probabilities = {"NN":.15,"NNS":.1,"RB":.1,"JJ":.1, "VBD":.1}
 
 def spaced(word):
-	if word in [",", ".", "?", "!", ":"]:
+	if word in ["", ".", "?", "!", ":"]:
 		return word
 	else:
 		return " " + word
-
+print("".join([spaced(x) for x in txt]))
 final_words = []
 
 
@@ -59,4 +44,7 @@ for (word, tag) in tagged_tokens:
 		new_word = input("Please enter %s:\n" % (tagmap[tag]))
 		final_words.append(spaced(new_word))
 
+
 print ("".join(final_words))
+
+print("\n\nEND*******")
